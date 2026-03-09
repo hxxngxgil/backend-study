31b112a70cd33cdd3886e5a82f698e5e97eefddb
tnsrlf13 <tnsrlf13@naver.com>
2026-03-09 21:33:37 +0900

    feat: java scope examples

---
+++ 01-java/basic/scope/Scope2.java
@@ -0,0 +1,16 @@
+package scope;
+
+public class Scope2 {
+
+    public static void main(String[] args) {
+        int m = 10;
+
+        for (int i = 0; i < 2; i++) {   //for문 블록 내부
+            System.out.println("for m = " + m);
+            System.out.println("for i = " + i);
+        }   //i 생존 종료
+
+        System.out.println("for m = " + m);
+        //System.out.println("for i = " + i); //i 접근 불가
+    }
+}
