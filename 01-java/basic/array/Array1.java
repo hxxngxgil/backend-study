ae15229e5c2aa6e989a6adbcde3a917e79554667
tnsrlf13 <tnsrlf13@naver.com>
2026-03-12 22:31:40 +0900

    feat: java array examples

---
+++ 01-java/basic/array/Array1.java
@@ -0,0 +1,18 @@
+package array;
+
+public class Array1 {
+
+    public static void main(String[] args) {
+        int student1 = 90;
+        int student2 = 90;
+        int student3 = 90;
+        int student4 = 90;
+        int student5 = 90;
+
+        System.out.println("학생1 점수: " + student1);
+        System.out.println("학생2 점수: " + student2);
+        System.out.println("학생3 점수: " + student3);
+        System.out.println("학생4 점수: " + student4);
+        System.out.println("학생5 점수: " + student5);
+    }
+}
---
+++ 01-java/basic/array/ArrayDi4.java
@@ -0,0 +1,25 @@
+package array;
+
+public class ArrayDi4 {
+
+    public static void main(String[] args) {
+        //2x3 2차원 배열
+        int[][] arr = new int[2][3];
+
+        int i = 1;
+
+        //1증가하는 값 대입
+        for (int row = 0; row < arr.length; row++) {
+            for (int column = 0; column < arr[row].length; column++) {
+                arr[row][column] = i++;
+            }
+        }
+
+        for (int row = 0; row < arr.length; row++) {
+            for (int column = 0; column < arr[row].length; column++) {
+                System.out.print(arr[row][column] + " ");
+            }
+            System.out.println();   //한 행이 끝나면 라인을 변경
+        }
+    }
+}
