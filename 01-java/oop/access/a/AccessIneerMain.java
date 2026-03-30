c4a22abc0fc0d3a85d020bb2cc9605bed21b0796
tnsrlf13 <tnsrlf13@naver.com>
2026-03-30 19:24:47 +0900

    feat: java access examples

---
+++ 01-java/oop/access/a/AccessIneerMain.java
@@ -0,0 +1,22 @@
+package access.a;
+
+public class AccessIneerMain {
+
+    public static void main(String[] args) {
+        AccessData data = new AccessData();
+
+        //public 호출 가능
+        data.publicField = 1;
+        data.publicMethod();
+
+        //같은 패키지 default 호출 가능
+        data.defaultField = 2;
+        data.defalutMethod();
+
+        //private 호출 불가
+        //data.privateField = 3;
+        //data.privateMethod();
+
+        data.innerAccess();
+    }
+}
